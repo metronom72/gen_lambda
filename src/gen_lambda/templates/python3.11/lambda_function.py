476883b428b84885d91b72b31c6ccf4be7ad25f7
Dorokhovich Michael <mikhail@dorokhovich.ru>
2025-02-23 19:24:08 +0100

    fix: dummy python lambda - Mikhail Dorokhovich

--- src/gen_lambda/templates/python3.11/lambda_function.py
+++ src/gen_lambda/templates/python3.11/lambda_function.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps({
-            message: "Lambda Result",
-            event: event
+            "message": "Lambda Result",
+            "event": event
         })
     }
